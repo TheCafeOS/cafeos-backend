298fb3f08d8c51a0fcb1b3fa40f383cbdd653aae
Mayank1343 <mayanksharma1343@gmail.com>
2026-07-06 23:34:24 +0530

    fix(public): forward async controller errors to global error handler

--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -1,10 +1,28 @@
-import { Router } from 'express';
-import { getPublicMenu, createPublicOrder, getPublicOrder } from '../controllers/publicController.js';
+import { Router } from "express";
+
+import {
+  getPublicMenu,
+  createPublicOrder,
+  getPublicOrder,
+} from "../controllers/publicController.js";
+
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const router = Router();
 
-router.get('/menu/:qrToken', getPublicMenu);
-router.post("/orders/:qrToken", createPublicOrder);
-router.get('/orders/:qrToken/:orderId', getPublicOrder);
+router.get(
+  "/menu/:qrToken",
+  asyncHandler(getPublicMenu),
+);
+
+router.post(
+  "/orders/:qrToken",
+  asyncHandler(createPublicOrder),
+);
+
+router.get(
+  "/orders/:qrToken/:orderId",
+  asyncHandler(getPublicOrder),
+);
 
 export default router;
