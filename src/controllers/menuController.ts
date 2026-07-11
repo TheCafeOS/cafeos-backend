38715361f03bb97d1e507160d0e823f6b24f86fc
Mayank1343 <mayanksharma1343@gmail.com>
2026-07-11 15:13:32 +0530

    refactor(menu): align controller with shared request utilities

--- src/controllers/menuController.ts
+++ src/controllers/menuController.ts
@@ -2,6 +2,7 @@ import { Response } from "express";
 import { AuthenticatedRequest } from "../middleware/auth.js";
 import * as menuService from "../services/menu.service.js";
 import { successResponse } from "../utils/apiResponse.js";
+import { getRouteParam } from "../utils/request.js";
 
 export const listMenuItems = async (
   req: AuthenticatedRequest,
@@ -40,9 +41,7 @@ export const updateMenuItem = async (
   req: AuthenticatedRequest,
   res: Response,
 ) => {
-  const id = Array.isArray(req.params.id)
-    ? req.params.id[0]
-    : req.params.id;
+  const id = getRouteParam(req.params.id);
 
   const menuItem = await menuService.editMenuItem(
     req.employee!.restaurantId,
@@ -62,11 +61,9 @@ export const deleteMenuItem = async (
   req: AuthenticatedRequest,
   res: Response,
 ) => {
-  const id = Array.isArray(req.params.id)
-    ? req.params.id[0]
-    : req.params.id;
+  const id = getRouteParam(req.params.id);
 
-  const deleted = await menuService.removeMenuItem(
+  await menuService.removeMenuItem(
     req.employee!.restaurantId,
     id,
   );
@@ -84,9 +81,7 @@ export const uploadMenuItemImage = async (
   req: AuthenticatedRequest,
   res: Response,
 ) => {
-  const id = Array.isArray(req.params.id)
-    ? req.params.id[0]
-    : req.params.id;
+  const id = getRouteParam(req.params.id);
 
   const menuItem = await menuService.uploadMenuImage(
     req.employee!.restaurantId,
